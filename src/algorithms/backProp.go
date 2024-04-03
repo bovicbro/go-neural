162ea0a81d517a3553b645f082315d6cca1c2dfb
Victor Broman <victor.bo.broman@gmail.com>
2024-04-03 21:00:17 +0200

    adds

--- src/algorithms/backProp.go
+++ src/algorithms/backProp.go
@@ -2,10 +2,9 @@ package Algorithms
 
 import "goNeural/dataStructure"
 
-type BackProp interface {
-	bp(n NeuralNetwork.Network) NeuralNetwork.Network
-}
-
+//	type BackProp interface {
+//		bp(n NeuralNetwork.Network) NeuralNetwork.Network
+//	}
 func bp(n NeuralNetwork.Network) NeuralNetwork.Network {
 	return n
 }
